ad6c41bdb5a488b82b6b214e0c44b415e9b6a58f
Manoj A N <manojhustler@gmail.com>
2023-05-19 08:51:44 +0530

    display js output in html

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
-let choice = ['Rock', 'Paper', 'Scissors'];
+const choice = ['Rock', 'Paper', 'Scissors'];
 
 const rockBtn = document.querySelector("#rockImage");
 const paperBtn = document.querySelector("#paperImage");
 const scissorsBtn = document.querySelector("#scissorsImage");
+const para = document.querySelector("p");
 
-
-function playedRock() {
-    let userChoice = "Rock"
+rockBtn.addEventListener("click", () =>{
+    let userChoice = "Rock";
     let compChoice = choice[Math.floor(Math.random() * choice.length)];
-        if (userChoice == "Rock" && compChoice == "Rock") {
-            console.log(`It's a tie. You both chose ${compChoice}.`);
-        } else if (userChoice == "Rock" && compChoice == "Paper") {
-            console.log(`Manoj wins! You chose Rock and he chose ${compChoice}.`);
-        } else if (userChoice == "Rock" && compChoice == "Scissors") {
-            console.log(`You win! Manoj chose ${compChoice} and you chose Rock.`);
-        } else {
-            console.log(`Nothing!`);
-        }
-}
-function playedPaper() {
-    let userChoice = "Paper"
-    let compChoice = choice[Math.floor(Math.random() * choice.length)];
-        if (userChoice == "Paper" && compChoice == "Paper") {
-            console.log(`It's a tie. You both chose ${compChoice}.`);
-        } else if (userChoice == "Paper" && compChoice == "Scissors") {
-            console.log(`Manoj wins! You chose Paper and he chose ${compChoice}.`);
-        } else if (userChoice == "Paper" && compChoice == "Rock") {
-            console.log(`You win! Manoj chose ${compChoice} and you chose Paper.`);
-        } else {
-            console.log(`Nothing!`);
-        }
-}
-function playedScissors() {
-    let userChoice = "Scissors"
-    let compChoice = choice[Math.floor(Math.random() * choice.length)];
-        if (userChoice == "Scissors" && compChoice == "Scissors") {
-            console.log(`It's a tie. You both chose ${compChoice}.`);
-        } else if (userChoice == "Scissors" && compChoice == "Rock") {
-            console.log(`Manoj wins! You chose Scissors and he chose ${compChoice}.`);
-        } else if (userChoice == "Scissors" && compChoice == "Paper") {
-            console.log(`You win! Manoj chose ${compChoice} and you chose Scissors.`);
-        } else {
-            console.log(`Nothing!`);
-        }
-}
+    
+    if (userChoice == "Rock" && compChoice == "Rock") {
+        para.textContent = "It's a tie. You both chose Rock.";
+    } else if (userChoice == "Rock" && compChoice == "Paper") {
+        para.textContent = "Manoj wins! You chose Rock and he chose Paper.";
+    } else if (userChoice == "Rock" && compChoice == "Scissors") {
+        para.textContent = "You win! Manoj chose Scissors and you chose Rock.";
+    } else {
+        para.textContent = "";
+    }
+});
 
-rockBtn.addEventListener("click", playedRock);
-paperBtn.addEventListener("click", playedPaper);
-scissorsBtn.addEventListener("click", playedScissors);
+paperBtn.addEventListener("click", () =>{
+    let userChoice = "Paper";
+    let compChoice = choice[Math.floor(Math.random() * choice.length)];
+    
+    if (userChoice == "Paper" && compChoice == "Paper") {
+        para.textContent = "It's a tie. You both chose Paper.";
+    } else if (userChoice == "Paper" && compChoice == "Scissors") {
+        para.textContent = "Manoj wins! You chose Paper and he chose Scissors.";
+    } else if (userChoice == "Paper" && compChoice == "Rock") {
+        para.textContent = "You win! Manoj chose Rock and you chose Paper.";
+    } else {
+        para.textContent = "";
+    }
+});
 
+scissorsBtn.addEventListener("click", () =>{
+    let userChoice = "Scissors";
+    let compChoice = choice[Math.floor(Math.random() * choice.length)];
+    
+    if (userChoice == "Scissors" && compChoice == "Scissors") {
+        para.textContent = "It's a tie. You both chose Scissors.";
+    } else if (userChoice == "Scissors" && compChoice == "Rock") {
+        para.textContent = "Manoj wins! You chose Scissors and he chose Rock.";
+    } else if (userChoice == "Scissors" && compChoice == "Paper") {
+        para.textContent = "You win! Manoj chose Paper and you chose Scissors.";
+    } else {
+        para.textContent = "";
+    }
+});
 
 
 
