e96f799c35749c72d054a1f48c60945b943f8b75
Manoj A N <manojhustler@gmail.com>
2023-05-20 17:33:04 +0530

    finalize this project

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 
 const choice = ['Rock', 'Paper', 'Scissors'];
-const manojWish = ['Take Manoj for a date', 'Take him for a nightout','Go for a long walk with him', 'Buy Manoj an ice-cream', 'Propose Manoj', 'Sing a song for Manoj', 'Go breakup with your boyfriend', 'Send Manoj your most embarrassing pic', 'Give Manoj 3 sincere compliments', 'Tell Manoj one of your darkest secrets'];
+const manojWish = ['Take Manoj for a date', 'Take him for a nightout','Go for a long walk with him', 'Buy Manoj an ice-cream', 'Go propose Manoj', 'Sing a song for Manoj', 'Give Manoj a kiss', 'Send Manoj your most embarrassing pic', 'Give Manoj 3 sincere compliments', 'Tell Manoj one of your darkest secrets'];
 const yourWish =['Ask Manoj to take you for a date', 'Ask Manoj to take you for a nightout', 'Ask Manoj to take you for a long walk', 'Ask Manoj to get you an ice-cream', 'Ask Manoj to propose you', 'Ask Manoj to sing a song for you', 'Ask Manoj to breakup with his girlfriend', 'Ask Manoj to send you his favorite pic', 'Ask Manoj to give you 3 sincere compliments', 'Ask Manoj one of his darkest secrets'];
 
 const rockBtn = document.querySelector("#rockImage");
@@ -14,6 +14,7 @@ const para = document.querySelector("#paragraph");
 const yoursScore = document.querySelector("#your-Score");
 const manojsScore = document.querySelector("#manoj-Score");
 const result = document.querySelector("#finalResult");
+const refresh = document.querySelector("#refreshInstruction");
 
 let getManojWish = manojWish[Math.floor(Math.random() * manojWish.length)];
 let getYourWish = yourWish[Math.floor(Math.random() * yourWish.length)];
@@ -21,7 +22,7 @@ let getYourWish = yourWish[Math.floor(Math.random() * yourWish.length)];
 let yourScore = 0;
 let manojScore = 0;
 
-yoursScore.textContent = `Your Score = ${yourScore}`;
+yoursScore.textContent = `Zara's Score = ${yourScore}`;
 manojsScore.textContent = `Manoj's Score = ${manojScore}`;
 
 rockBtn.addEventListener("click", () =>{
@@ -30,36 +31,42 @@ rockBtn.addEventListener("click", () =>{
 
     if (userChoice == "Rock" && compChoice == "Rock") {
         para.textContent = "It's a tie. You both chose Rock.";
-        yoursScore.textContent = `Your Score = ${yourScore.toString()}`;
+        yoursScore.textContent = `Zara's Score = ${yourScore.toString()}`;
         manojsScore.textContent = `Manoj's Score = ${manojScore.toString()}`;
         if (yourScore == '5') {
             result.textContent = `You won the game!\n${getYourWish}.`;
+            refresh.textContent = `Refresh the page to play again`;
         } else if (manojScore == '5') {
             result.textContent = `Manoj won the game!\n${getManojWish}.`;
+            refresh.textContent = `Refresh the page to play again`;
         } else {
             result.textContent = "";
         }
     } else if (userChoice == "Rock" && compChoice == "Paper") {
         para.textContent = "Manoj wins! You chose Rock and he chose Paper.";
         manojScore++;
-        yoursScore.textContent = `Your Score = ${yourScore.toString()}`;
+        yoursScore.textContent = `Zara's Score = ${yourScore.toString()}`;
         manojsScore.textContent = `Manoj's Score = ${manojScore.toString()}`;
         if (yourScore == '5') {
             result.textContent = `You won the game!\n${getYourWish}.`;
+            refresh.textContent = `Refresh the page to play again`;
         } else if (manojScore == '5') {
             result.textContent = `Manoj won the game!\n${getManojWish}.`;
+            refresh.textContent = `Refresh the page to play again`;
         } else {
             result.textContent = "";
         }
     } else if (userChoice == "Rock" && compChoice == "Scissors") {
         para.textContent = "You win! Manoj chose Scissors and you chose Rock.";
         yourScore++;
-        yoursScore.textContent = `Your Score = ${yourScore.toString()}`;
+        yoursScore.textContent = `Zara's Score = ${yourScore.toString()}`;
         manojsScore.textContent = `Manoj's Score = ${manojScore.toString()}`;
         if (yourScore == '5') {
             result.textContent = `You won the game!\n${getYourWish}.`;
+            refresh.textContent = `Refresh the page to play again`;
         } else if (manojScore == '5') {
             result.textContent = `Manoj won the game!\n${getManojWish}.`;
+            refresh.textContent = `Refresh the page to play again`;
         } else {
             result.textContent = "";
         }
@@ -119,36 +126,42 @@ paperBtn.addEventListener("click", () =>{
     
     if (userChoice == "Paper" && compChoice == "Paper") {
         para.textContent = "It's a tie. You both chose Paper.";
-        yoursScore.textContent = `Your Score = ${yourScore.toString()}`;
+        yoursScore.textContent = `Zara's Score = ${yourScore.toString()}`;
         manojsScore.textContent = `Manoj's Score = ${manojScore.toString()}`;
         if (yourScore == '5') {
             result.textContent = `You won the game!\n${getYourWish}.`;
+            refresh.textContent = `Refresh the page to play again`;
         } else if (manojScore == '5') {
             result.textContent = `Manoj won the game!\n${getManojWish}.`;
+            refresh.textContent = `Refresh the page to play again`;
         } else {
             result.textContent = "";
         }
     } else if (userChoice == "Paper" && compChoice == "Scissors") {
         para.textContent = "Manoj wins! You chose Paper and he chose Scissors.";
         manojScore++;
-        yoursScore.textContent = `Your Score = ${yourScore.toString()}`;
+        yoursScore.textContent = `Zara's Score = ${yourScore.toString()}`;
         manojsScore.textContent = `Manoj's Score = ${manojScore.toString()}`;
         if (yourScore == '5') {
             result.textContent = `You won the game!\n${getYourWish}.`;
+            refresh.textContent = `Refresh the page to play again`;
         } else if (manojScore == '5') {
             result.textContent = `Manoj won the game!\n${getManojWish}.`;
+            refresh.textContent = `Refresh the page to play again`;
         } else {
             result.textContent = "";
         }
     } else if (userChoice == "Paper" && compChoice == "Rock") {
         para.textContent = "You win! Manoj chose Rock and you chose Paper.";
         yourScore++;
-        yoursScore.textContent = `Your Score = ${yourScore.toString()}`;
+        yoursScore.textContent = `Zara's Score = ${yourScore.toString()}`;
         manojsScore.textContent = `Manoj's Score = ${manojScore.toString()}`;
         if (yourScore == '5') {
             result.textContent = `You won the game!\n${getYourWish}.`;
+            refresh.textContent = `Refresh the page to play again`;
         } else if (manojScore == '5') {
             result.textContent = `Manoj won the game!\n${getManojWish}.`;
+            refresh.textContent = `Refresh the page to play again`;
         } else {
             result.textContent = "";
         }
@@ -163,36 +176,42 @@ scissorsBtn.addEventListener("click", () =>{
     
     if (userChoice == "Scissors" && compChoice == "Scissors") {
         para.textContent = "It's a tie. You both chose Scissors.";
-        yoursScore.textContent = `Your Score = ${yourScore.toString()}`;
+        yoursScore.textContent = `Zara's Score = ${yourScore.toString()}`;
         manojsScore.textContent = `Manoj's Score = ${manojScore.toString()}`;
         if (yourScore == '5') {
             result.textContent = `You won the game!\n${getYourWish}.`;
+            refresh.textContent = `Refresh the page to play again`;
         } else if (manojScore == '5') {
             result.textContent = `Manoj won the game!\n${getManojWish}.`;
+            refresh.textContent = `Refresh the page to play again`;
         } else {
             result.textContent = "";
         }
     } else if (userChoice == "Scissors" && compChoice == "Rock") {
         para.textContent = "Manoj wins! You chose Scissors and he chose Rock.";
         manojScore++;
-        yoursScore.textContent = `Your Score = ${yourScore.toString()}`;
+        yoursScore.textContent = `Zara's Score = ${yourScore.toString()}`;
         manojsScore.textContent = `Manoj's Score = ${manojScore.toString()}`;
         if (yourScore == '5') {
             result.textContent = `You won the game!\n${getYourWish}.`;
+            refresh.textContent = `Refresh the page to play again`;
         } else if (manojScore == '5') {
             result.textContent = `Manoj won the game!\n${getManojWish}.`;
+            refresh.textContent = `Refresh the page to play again`;
         } else {
             result.textContent = "";
         }
     } else if (userChoice == "Scissors" && compChoice == "Paper") {
         para.textContent = "You win! Manoj chose Paper and you chose Scissors.";
         yourScore++;
-        yoursScore.textContent = `Your Score = ${yourScore.toString()}`;
+        yoursScore.textContent = `Zara's Score = ${yourScore.toString()}`;
         manojsScore.textContent = `Manoj's Score = ${manojScore.toString()}`;
         if (yourScore == '5') {
             result.textContent = `You won the game!\n${getYourWish}.`;
+            refresh.textContent = `Refresh the page to play again`;
         } else if (manojScore == '5') {
             result.textContent = `Manoj won the game!\n${getManojWish}.`;
+            refresh.textContent = `Refresh the page to play again`;
         } else {
             result.textContent = "";
         }
